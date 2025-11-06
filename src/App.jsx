2d4f6bd1ec9c1efde03aa5f5f7cfb95486a9af82
aribatariq231 <aribatariq231@gmail.com>
2025-11-06 12:52:53 +0500

    WeatherApp-CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import { useState } from "react";
- import './App.css';
+import { useState , useEffect} from "react";
+import './App.css';
 import axios from "axios";
 
 function App(){
-const[weather, setWeather]=useState({})
+const[cityName, setCityName] = useState();
+const[weather, setWeather]=useState({});
+const [cities, setCities] =  useState({});
 
-const[cityName, setCityName] = useState()
   const handleChange=(e)=>{
     setCityName(e.target.value)
   }
+
+  useEffect(()=>{
+    if(weather.current){
+      alert("Something went wrong");
+    }
+  },[weather])
+
+  
+  useEffect(()=>{
+  const fetchWeather = async ()=>{
+  const cities=["Lahore Pakistan", "Karachi Pakistan"]
+  const Weatherinfo = []
+  cities.map(async(city)=>{
+  console.log(city)
+  const res = await axios.get(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${city}`)
+  console.log(res.data)
+  Weatherinfo.push(res.data)
+  })
+  setCities(Weatherinfo)
+  }
+  fetchWeather()
+  },[])
   
   const handleSubmit= async(e)=>{
     e.preventDefault()
     // alert("Submit Successfully!")
     try{
-      const res = await axios.get(`https:p2pclouds.up.railway.app/v1/learn/weather?city=${cityName}`)
+      const res = await axios.get(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${cityName}`)
       setWeather(res.data)
     }
     catch(err){
@@ -34,11 +57,19 @@ const[cityName, setCityName] = useState()
     </div>
     <button type="submit">Get</button>
   </form>
-  <div>
-    <h1>{weather.location && weather.location.name}</h1>
-    <h1>{weather.location && weather.location.region}</h1>
-    <h1>{weather.current && weather.current.temp_c}</h1>
-  </div>
+  {
+    weather.current && <div className="weather-card">
+      <div className="card-img-container">
+        <img src="/lhr-img.jpg" alt="" className="card-img" />
+      </div>
+      <div className="card-title">
+        <h1>{weather.current && weather.current.temp_c}</h1>
+      </div>
+      <div className="card-desc">
+        <p>{weather.location && weather.location.name},{weather.location && weather.location.name}</p>
+      </div>
+    </div>
+  }
     </>
   )
 }
